40d63c438f685256c73860b87b6ac70c87472c83
endreodev <endreo.dev@gmail.com>
2024-06-23 18:52:22 -0400

    atualizar dados

--- app/config.py
+++
@@ -1,5 +0,0 @@
-def configure_app(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Jaca@157@localhost/homebroker'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = 'homebrokerENDREO@2024'
-    app.config['CORS_HEADERS'] = 'Content-Type'
